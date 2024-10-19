22f3e5defaf7bb7bc31ef44e44350c4f0f4944f4
Dylan-KA <111679436+Dylan-KA@users.noreply.github.com>
2024-10-19 15:22:36 +1100

    Initial Share-Extension
    From a photo, users can tap Share > Travel Journal App

---
+++ Share-Extension/ShareView.swift
@@ -0,0 +1,68 @@
+//
+//  ShareView.swift
+//  Share-Extension
+//
+//  Created by Dylan Archer on 19/10/2024.
+//
+
+import SwiftUI
+
+struct ShareView: View {
+    
+    @State private var image: UIImage
+    @State private var text: String
+        
+        init(image: UIImage) {
+            self.image = image
+            self.text = ""
+        }
+        
+        var body: some View {
+            NavigationStack{
+                VStack(spacing: 20){
+                    if let imageData = image.pngData(),
+                       let uiImage = UIImage(data: imageData) {
+                        Image(uiImage: uiImage)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    } else {
+                        Text("Unable to load image")
+                    }
+                    
+                    TextField("Journal", text: $text, axis: .vertical)
+                        .lineLimit(3...6)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    Button {
+                        // TODO: save image to journal
+                        close()
+                    } label: {
+                        Text("Save")
+                            .frame(maxWidth: .infinity)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Spacer()
+                }
+                .padding()
+                .navigationTitle("Share Extension")
+                .toolbar {
+                    Button("Cancel") {
+                        close()
+                    }
+                }
+            }
+        }
+    
+    // Notifies ShareViewController to close the view
+    func close() {
+        NotificationCenter.default.post(name: NSNotification.Name("close"), object: nil)
+    }
+}
+
+/*
+ #Preview {
+ ShareView(image: UIImage)
+ }
+ */
