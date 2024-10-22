884d904ab7c2222ef8033ed75916c0926cf5091a
Byron Lester <125940003+byro23@users.noreply.github.com>
2024-10-22 18:01:45 +1100

    implemented basic map

--- Travel Journal App/ViewModels/MapViewModel.swift
+++ Travel Journal App/ViewModels/MapViewModel.swift
@@ -6,7 +6,14 @@
 //
 
 import Foundation
+import MapKit
 
 class MapViewModel: ObservableObject {
     
+    private static let defaultRegion = CLLocationCoordinate2D(latitude: -33.8688, longitude: 151.2093)
+    
+    
+    @Published var region: MKCoordinateRegion = MKCoordinateRegion(
+        center: defaultRegion, span: MKCoordinateSpan(latitudeDelta: 0.4, longitudeDelta: 0.4)
+    )
 }
--- Travel Journal App/Views/MapView.swift
+++ Travel Journal App/Views/MapView.swift
@@ -9,12 +9,23 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
+    @StateObject var viewModel = MapViewModel()
+    
+    let initialPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: -33.8688, longitude: 151.2093),
+            span: MKCoordinateSpan(latitudeDelta: 0.4, longitudeDelta: 0.4)
+        )
+    )
+    
     var body: some View {
         
         VStack{
             HeaderView()
             
-            Map()
+            Map(initialPosition: initialPosition) {
+                
+            }
         }
         
         
