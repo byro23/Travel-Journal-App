23497010a4581521a181ab23843c32dc53ed1e81
Byron Lester <125940003+byro23@users.noreply.github.com>
2024-10-29 15:10:38 +1100

    Update NewJournalView.swift

--- Travel Journal App/Views/NewJournalView.swift
+++ Travel Journal App/Views/NewJournalView.swift
@@ -284,6 +284,7 @@ struct NewJournalView: View {
                 .background(Color(.systemGroupedBackground))
                 .onAppear {
                     viewModel.fetchNearbyPlaces()
+                    mapViewModel.tappedCoordinates = nil
                 }
                 // append selected images to images array
                 .onChange(of: photosPickerItems){ _, _ in
